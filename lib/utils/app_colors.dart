2b0c9cbfe971eb8e23935e87039b79a1dc3860f3
MemoonaKhalid76 <khalidmamona563@gmail.com>
2026-01-05 14:07:23 +0500

    Delete lib directory

--- lib/utils/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xFF2E3A59);
-  static const background = Color(0xFFF7F3F2);
-  static const card = Colors.white;
-  static const textDark = Colors.black87;
-}
