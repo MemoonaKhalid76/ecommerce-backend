c391371b37d7725e9dfd701c9441f198517eecd9
MemoonaKhalid76 <khalidmamona563@gmail.com>
2026-01-19 11:55:35 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
-    console.error(error);
-    process.exit(1);
+    console.error('MongoDB Connection Error:', error);
+    // process.exit(1); // Remove this for Vercel
   }
 };
 
